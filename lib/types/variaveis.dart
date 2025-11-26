480ba838f806cdd226e34d9e888130ebd6ca3da8
David Santos <davidhenriquehy@gmail.com>
2025-11-26 13:29:05 -0300

    variaveis

---
+++ lib/types/variaveis.dart
@@ -0,0 +1,15 @@
+void main() {
+  var nome = "David";
+  var idade = 27;
+  var peso = 83.5;
+  var verdadeiro = true;
+  print(nome);
+  print(idade);
+  print(peso);
+  print(verdadeiro);
+  final altura;
+  altura = 1.75;
+  late String nomePai;
+  nomePai = "Carlos";
+   
+}
